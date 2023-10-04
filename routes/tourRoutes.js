320865548df2546171b25be32236b8e78908f072
amyspencerproject <amy@spencerproject.com>
2023-10-04 14:58:03 -0600

    Add a chained middleware for creating a tour post route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,10 +6,15 @@ const router = express.Router();
 
 router.param('id', tourController.checkId);
 
+// create a checkBody middleware function
+// check if body contains the name and price property
+// if not, send back 400 (invalid request)
+// add it to the post handler stack
+
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createNewTour);
+  .post(tourController.checkBody, tourController.createNewTour);
 
 router
   .route('/:id')
