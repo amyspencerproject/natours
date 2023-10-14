20ec7e9749a1b5dbe1eb85eb7b2f98f52ff4ba2e
amyspencerproject <amy@spencerproject.com>
2023-10-13 18:17:26 -0600

    Add Mongoose and create a schema and model

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
-dotenv.config({ path: './config.env' });
-
 const app = require('./app');
 // console.log(app.get('env'));
 // console.log(process.env);
 
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFinderAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+console.log(Tour);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
